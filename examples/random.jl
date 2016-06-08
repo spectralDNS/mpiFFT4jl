e2b514736fa442cd00dd7389618b6f95506e8432
Mikael Mortensen <mikaem@math.uio.no>
2016-06-08 15:05:45 +0200

    Adding functionality

--- examples/random.jl
+++ examples/random.jl
@@ -7,23 +7,26 @@ function foo(comm::MPI.Comm)
     const N = [32, 64, 64]    # Global shape of mesh
     const L = [2pi, 2pi, 2pi] # Real size of mesh
     
-    FFT = mpiFFT4jl.slab.SlabFFT(N, L, comm)
+    FFT = mpiFFT4jl.slab.r2c(N, L, comm)
 
     # Create a random array and its transform
     U = rand(Float64, mpiFFT4jl.slab.real_shape(FFT))
-    U_hat = Array{Complex{Float64}}(mpiFFT4jl.slab.complex_shape(FFT))
+    U_hat  = Array{Complex{Float64}}(mpiFFT4jl.slab.complex_shape(FFT))
+    U_hat2 = similar(U_hat)
     U2 = similar(U)
+    U3 = similar(U)
+    
+    mpiFFT4jl.slab.rfft3(FFT, U_hat, U)
 
-    for i in 1:60
-        mpiFFT4jl.slab.rfft3(FFT, U_hat, U)
+    for i in 1:10
         mpiFFT4jl.slab.irfft3(FFT, U2, U_hat)
+#         mpiFFT4jl.slab.irfft3(FFT, U3, U_hat, 3)
     end
 
-    MPI.Reduce(sumabs2(U2-U), MPI.SUM, 0, comm) 
-    
+    (MPI.Reduce(sumabs2(U2-U), MPI.SUM, 0, comm), FFT)
 end
 
 comm = MPI.COMM_WORLD
-@time k = foo(comm)
-if MPI.Comm_rank(comm) == 0 println("Error = $(k)") end
+@time (k, FFT) = foo(comm)
+if MPI.Comm_rank(comm) == 0 println("Error  = $(k)") end
 MPI.Finalize()
--- src/slab.jl
+++ src/slab.jl
@@ -1,14 +1,22 @@
 using MPI
+using utils
 
-export rfft3, irfft3, real_shape, complex_shape, SlabFFT
+# export rfft3, irfft3, real_shape, complex_shape, r2c
+export *
 
 # Spectral transformation of three dimensional data aligned
 # such that the last component is parallelized across processes
-immutable SlabFFT{T<:Real}
+type r2c{T<:Real}
     # Global shape
     N::Array{Int, 1}
     # Global size of domain
     L::Array{T, 1}
+    # Communicator
+    comm::MPI.Comm
+    num_processes::Int
+    rank::Int
+    chunk::Int    # Amount of data to be send by MPI
+    
     # Plans
     plan12::FFTW.rFFTWPlan{T}
     plan3::FFTW.cFFTWPlan
@@ -19,14 +27,10 @@ immutable SlabFFT{T<:Real}
     v_view::Array{Complex{T}, 4}
     v_recv::Array{Complex{T}, 3}
     v_recv_view::Array{Complex{T}, 4}
-    # Communicator
-    comm::MPI.Comm
-    # Amount of data to be send by MPI
-    chunk::Int    
-    num_processes::Int
+    dealias::Array{Int, 1}
 
     # Constructor
-    function SlabFFT(N, L, comm)
+    function r2c(N, L, comm)
         # Verify input
         Nh = N[1]÷2+1
         p = MPI.Comm_size(comm)
@@ -40,42 +44,140 @@ immutable SlabFFT{T<:Real}
 
         # Plan Fourier transformations
         A = zeros(T, (N[1], N[2], Np[3]))
-        plan12 = plan_rfft(A, (1, 2))
-        plan3 = plan_fft!(v, (3, ))
+        if p > 1
+            plan12 = plan_rfft(A, (1, 2))
+            plan3 = plan_fft!(v, (3, ))
+        else  # Use only plan12 to do entire transform
+            plan12 = plan_rfft(A, (1, 2, 3))
+            plan3 = plan_fft!(zeros(Complex{Float64}, 2,2,2), (1, 2, 3))
+        end
+
         # Compute the inverse plans
-        inv(plan12); inv(plan3)
+        inv(plan12)
+        if p > 1 inv(plan3) end
 
         chunk = Nh*Np[2]*Np[3]
         # Now we are ready
-        new(N, L, plan12, plan3,
-            vT, vT_view, v, v_view, v_recv, v_recv_view,
-            comm, chunk, p)
+        new(N, L, comm, p, MPI.Comm_rank(comm), chunk,
+            plan12, plan3,
+            vT, vT_view, v, v_view, v_recv, v_recv_view)
     end
 end
 
 # Constructor
-SlabFFT{T<:Real}(N::Array{Int, 1}, L::Array{T, 1}, comm::Any) = SlabFFT{T}(N, L, comm)
+r2c{T<:Real}(N::Array{Int, 1}, L::Array{T, 1}, comm::Any) = r2c{T}(N, L, comm)
 
 # Transform real to complex as complex = T o real
-function rfft3{T<:Real}(F::SlabFFT{T}, fu::AbstractArray{Complex{T}, 3}, u::AbstractArray{T})
-    A_mul_B!(F.vT, F.plan12, u)
-    permutedims!(F.v_view, F.vT_view, [1, 2, 4, 3])
-    MPI.Alltoall!(F.v_recv_view, F.v_view, F.chunk, F.comm)
-    F.plan3*F.v_recv; fu[:] = F.v_recv
+function rfft3{T<:Real}(F::r2c{T}, fu::AbstractArray{Complex{T}, 3}, u::AbstractArray{T})
+    if F.num_processes > 1
+        A_mul_B!(F.vT, F.plan12, u)
+        permutedims!(F.v_view, F.vT_view, [1, 2, 4, 3])
+        MPI.Alltoall!(F.v_recv_view, F.v_view, F.chunk, F.comm)
+        F.plan3*F.v_recv; fu[:] = F.v_recv
+    else
+        A_mul_B!(fu, F.plan12, u)
+    end
 end
 
 # Transform complex to real as real = T o complex
-function irfft3{T<:Real}(F::SlabFFT{T}, u::AbstractArray{T}, fu::AbstractArray{Complex{T}, 3})
-    F.plan3.pinv*fu; F.v[:] = fu
-    MPI.Alltoall!(F.v_recv_view, F.v_view, F.chunk, F.comm)
-    permutedims!(F.vT_view, F.v_recv_view, [1, 2, 4, 3])
-    A_mul_B!(u, F.plan12.pinv, F.vT)
+function irfft3{T<:Real}(F::r2c{T}, u::AbstractArray{T}, fu::AbstractArray{Complex{T}, 3}, dealias_fu::Int=0)
+    if F.num_processes > 1
+        F.v[:] = fu
+        if dealias_fu == 1
+            dealias(F, F.v)
+        elseif dealias_fu == 2
+            dealias2(F, F.v)
+        end
+        F.plan3.pinv*F.v
+        MPI.Alltoall!(F.v_recv_view, F.v_view, F.chunk, F.comm)
+        permutedims!(F.vT_view, F.v_recv_view, [1, 2, 4, 3])
+        A_mul_B!(u, F.plan12.pinv, F.vT)
+    else
+        A_mul_B!(u, F.plan12.pinv, fu)
+    end
 end
 
-function real_shape{T<:Real}(F::SlabFFT{T})
-    return (F.N[1], F.N[2], F.N[3]÷F.num_processes)
+function real_shape{T<:Real}(F::r2c{T})
+    (F.N[1], F.N[2], F.N[3]÷F.num_processes)
 end
 
-function complex_shape{T<:Real}(F::SlabFFT{T})
-    return (F.N[1]÷2+1, F.N[2]÷F.num_processes, F.N[3])
+function complex_shape{T<:Real}(F::r2c{T})
+    (F.N[1]÷2+1, F.N[2]÷F.num_processes, F.N[3])
 end
+
+function complex_shape_T{T<:Real}(F::r2c{T})
+    (F.N[1]÷2+1, F.N[2], F.N[3]÷F.num_processes)
+end
+
+function complex_local_slice{T<:Real}(F::r2c{T})
+    ((1, F.N[1]÷2+1), 
+     (F.rank*F.N[2]÷F.num_processes+1, (F.rank+1)*F.N[2]÷F.num_processes),
+     (1, F.N[3]))
+end
+
+function complex_local_wavenumbers{T<:Real}(F::r2c{T})
+    (rfftfreq(F.N[1], 1.0/F.N[1]),
+     fftfreq(F.N[2], 1.0/F.N[2])[F.rank*div(F.N[2], F.num_processes)+1:(F.rank+1)*div(F.N[2], F.num_processes)],
+     fftfreq(F.N[3], 1.0/F.N[3]))
+end
+
+function get_local_wavenumbermesh{T<:Real}(F::r2c{T})
+    K = Array{Int}(tuple(push!([complex_shape(F)...], 3)...))
+    k = complex_local_wavenumbers(F)
+    for (i, Ki) in enumerate(ndgrid(k[1], k[2], k[3])) K[view(i)...] = Ki end
+    K
+end
+
+function get_local_mesh{T<:Real}(F::r2c{T})
+    # Real grid
+    x = collect(0:F.N[1]-1)*F.L[1]/F.N[1]
+    y = collect(0:F.N[2]-1)*F.L[2]/F.N[2]
+    z = collect(0:F.N[3]-1)*F.L[3]/F.N[3]
+    X = Array{T}(tuple(push!([real_shape(F)...], 3)...))
+    for (i, Xi) in enumerate(ndgrid(x, y, z[F.rank*F.N[3]÷F.num_processes+1:(F.rank+1)*F.N[3]])) X[view(i)...] = Xi end
+    X
+end    
+
+function dealias{T<:Real}(F::r2c{T}, fu::AbstractArray{Complex{T}, 3})
+    kk = complex_local_wavenumbers(F)
+    for (k, kz) in enumerate(kk[3])
+        x = false
+        if abs(kz) > div(F.N[3], 3)
+        @inbounds fu[:, :, k] = 0.0
+            continue
+        end
+        for (j, ky) in enumerate(kk[2])
+            if abs(ky) > div(F.N[2], 3)
+               @inbounds fu[:, j, k] = 0
+                continue
+            end
+            for (i, kx) in enumerate(kk[1])
+                if (abs(kx) > div(F.N[1], 3))
+                    @inbounds fu[i, j, k] = 0.0
+                end
+            end
+        end
+    end
+end
+
+function dealias2{T<:Real}(F::r2c{T}, fu::AbstractArray{Complex{T}, 3})
+    if  !isdefined(F, :dealias)
+        const kmax_dealias = F.N/3
+        K = get_local_wavenumbermesh(F)
+        (kx, ky, kz) = K[:,:,:,1], K[:,:,:,2], K[:,:,:,3]
+        indices = []
+        i = 1
+        for (x,y,z) in zip(kx, ky, kz)
+            if abs(x) > div(F.N[1], 3) || abs(y) > div(F.N[2], 3) || abs(z) > div(F.N[3], 3)
+                push!(indices, i)
+            end
+            i += 1
+        end
+        F.dealias = indices
+    end
+    for i in F.dealias
+      @inbounds  fu[i] = 0.0
+    end
+end
+
+
